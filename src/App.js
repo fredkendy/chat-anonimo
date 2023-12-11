128a50011b1cde39b19878106378a4a4b6ca2031
fredkendy <fredkendy9@gmail.com>
2023-12-11 15:48:01 -0300

    fazendo requisicao no backend do firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Mensagem from './components/Mensagem'
 import { Button, Input } from '@rneui/base';
+import database from '@react-native-firebase/database';
 
 export default function App() {
+
+  const [listaMensagens, setListaMensagens] = React.useState([])
+
+  React.useEffect(() => {
+    database()
+      .ref('/mensagens')
+      .on('value', snapshot => {
+        const mensagens = Object.values(snapshot.val());
+        console.log('Data: ', mensagens);
+        setListaMensagens(mensagens)
+      });
+  }, [])
+
   return (
     <View
       style={{padding: 18, display: 'flex', flexDirection: 'column', flex: 1}}>
@@ -12,11 +26,11 @@ export default function App() {
       <ScrollView>
         {/* Importando o componente Mensagem com propriedade right que indica 
       onde deve ser exibida (caso seja a propria pessoa mandando msg) */}
-        <View
-          style={{marginTop: 16, display: 'flex', flex: 1, overflow: 'scroll'}}>
-          <Mensagem />
-          <Mensagem right />
-          <Mensagem />
+        <View style={{marginTop: 16, display: 'flex', flex: 1, overflow: 'scroll'}}>
+          {
+            listaMensagens.map(data =>
+              <Mensagem />)
+          }
         </View>
       </ScrollView>
 
