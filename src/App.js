939dba3799dc3f5a127ff99e9b135f11d2aed41d
fredkendy <fredkendy9@gmail.com>
2023-12-11 14:17:22 -0300

    setup inicial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Chat Anonimo</Text>
+    </View>
+  )
+}
